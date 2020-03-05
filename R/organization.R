2f90cd2f375a5a08d599685f42e8c12b07ccf2c3
Drew Dolgert <adolgert@uw.edu>
2020-03-05 11:59:46 -0800

    made the new directory organization

--- R/organization.R
+++ R/organization.R
@@ -13,48 +13,116 @@
 #' @return a path to use as a base path for this dataset.
 #' @export
 ramp_path <- function(stage, location, project, user, rproject, path = NULL) {
-  if (stage == "input") {
-    if (!is.null(location)) {
-      base <- fs::path("countries", location)
-    } else if (!is.null(project)) {
-      base <- fs::path("projects", project)
-    } else if (!is.null(rproject)) {
-      base <- fs::path("rprojects", rproject)
+  stopifnot(stage %in% c("input", "output", "working"))
+
+  used <- list()
+  # Keep track of which tokens were used and which weren't.
+  using <- function(key) {
+    if (!is.null(key)) {
+      used[key] <<- TRUE
+      return(TRUE)
+    }
+    FALSE
+  }
+
+  using(stage)
+  if (using(project)) {
+    if (stage == "input") {
+      base <- fs::path("projects", project, "inputs")
+    } else if (stage == "output") {
+      base <- fs::path("projects", project, "outputs")
     } else {
-      stop("An input file needs a location, project, or rproject.")
+      # stage == working
+      if (using(user)) {
+        base <- fs::path("projects", project, "users", user)
+      } else {
+        stop("Must have username to put working data under a project.")
+      }
     }
-    final <- base
-  } else if (stage == "working") {
-    if (is.null(user)) {
-      stop("All working data is stored by username, so supply a user when stage=working.")
+  } else if (using(rproject)) {
+    base <- fs::path("libraries", rproject, "inst", "extdata")
+    if (using(user)) {
+      base <- fs::path(base, "users", user)
     }
-    base <- fs::path("users", user)
-    if (!is.null(project)) {
-      final <- fs::path(base, project)
-    } else if (!is.null(rproject)) {
-      final <- fs::path(base, rproject)
-    } else {
-      final <- base
-    }
-  } else if (stage == "output") {
-    if (!is.null(project)) {
-      base <- fs::path("projects", project)
-    } else if (!is.null(rproject)) {
-      base <- fs::path("rprojects", rproject)
-    } else {
-      stop("An output file needs a project, or rproject.")
-    }
-    final <- base
   } else {
-    stop(paste("stage should be one of (working, input, output) but is", stage))
+    if (stage == "input") {
+      base <- fs::path("inputs")
+    } else {
+      if (using(user)) {
+        base <- fs::path("users", user)
+      } else {
+        stop("Nowhere to put a file that has no project, rproject, or user
+             and is an output or working.")
+      }
+    }
   }
-  if (!is.null(path)) {
-    final <- fs::path(final, path)
+
+  # Location is tacked onto the end if it's used.
+  if (using(location)) {
+    base <- fs::path(base, "locations", location)
+  } # else don't tack a location on the end.
+  if (using(path)) {
+    base <- fs::path(base, path)
   }
-  final
+  list(path = base, used = used)
 }
 
 
+
+#' Turns a relative path into a ramp path.
+#'
+#' @param path A path relative to the base data directory.
+#' @return A list that is a ramp path.
+#' @examples
+#' inverse_ramp_path("users/ad") == list(stage = "working", user = "ad")
+#' @export
+inverse_ramp_path <- function(path) {
+  splitted <- fs::path_split(path)[[1]]
+  ramp_path <- list()
+  if (splitted[1] == "inputs") {
+    ramp_path["stage"] <- "input"
+    rest_idx <- 2
+  } else if (splitted[1] == "users") {
+    ramp_path["user"] <- splitted[2]
+    ramp_path["stage"] <- "working"
+    rest_idx <- 3
+  } else if (splitted[1] == "projects") {
+    ramp_path["project"] <- splitted[2]
+    if (splitted[3] == "inputs") {
+      ramp_path["stage"] <- "input"
+      rest_idx <- 4
+    } else if (splitted[3] == "outputs") {
+      ramp_path["stage"] <- "output"
+      rest_idx <- 4
+    } else if (splitted[3] == "users") {
+      ramp_path["stage"] <- "working"
+      ramp_path["user"] <- splitted[4]
+      rest_idx <- 5
+    } else {
+      stop("project directory must be inputs, outputs, or users")
+    }
+  } else if (splitted[1] == "libraries") {
+    ramp_path["rproject"] <- splitted[2]
+    ramp_path["stage"] <- "working"
+    if (length(splitted) > 4 & splitted[5] == "users") {
+      ramp_path["user"] <- splitted[6]
+      rest_idx <- 7
+    } else {
+      rest_idx <- 3
+    }
+  }
+
+  # add location
+  if (length(ramp_path) >= rest_idx & ramp_path[rest_idx] == "location") {
+    ramp_path["location"] <- ramp_path[rest_idx + 1]
+    rest_idx <- rest_idx + 2
+  }
+  # add path
+  if (length(ramp_path) >= rest_idx) {
+    ramp_path["path"] <- ramp_path[rest_idx:length(ramp_path)]
+  }
+  ramp_path
+}
 
 
 #' Creates a path relative to the project root, following
@@ -78,5 +146,5 @@ project_path <- function(file_id) {
   for (name in names(file_id)) {
     complete[[name]] <- file_id[[name]]
   }
-  do.call(ramp_path, complete)
+  do.call(ramp_path, complete)$path
 }
--- tests/testthat/test-organization.R
+++ tests/testthat/test-organization.R
@@ -1,4 +1,90 @@
-test_that("project path for just stage", {
-  pp <- project_path(list(stage = "input", location = "uganda"))
-  expect_equal(pp, "countries/uganda")
-})
+test_list <- function(trials) {
+  for (idx in 1:length(trials)) {
+    trial <- trials[[idx]]
+    trial_name <- paste("ramp_path obeys list", names(trials)[idx])
+
+    test_that(trial_name, {
+      had_error <- FALSE
+      pp <- tryCatch({
+        ramp_path(
+          stage = trial[["stage"]], location = NULL, project = trial[["project"]],
+          user = trial[["user"]], rproject = trial[["rproject"]]
+          )
+      }, error = function(e) had_error <<- TRUE
+      )
+      if (!is.null(trial[["result"]])) {
+        expect_equal(pp$path, trial[["result"]])
+        expect_equal(had_error, FALSE)
+      } else {
+        expect_equal(had_error, TRUE)
+      }
+    })
+  }
+}
+
+
+test_inverse_list <- function(trials) {
+  # Select those paths which don't cause exceptions.
+  trials <- trials[sapply(trials, function(x) !is.null(x$result))]
+
+  for (idx in 1:length(trials)) {
+    trial <- trials[[idx]]
+    trial_name <- paste("inverse_ramp_path parses path", names(trials)[idx])
+    expected <- trial[unlist(lapply(trial, function(x) !is.null(x)))]
+    expected <- expected[!names(expected) %in% "result"]
+    test_that(trial_name, {
+      had_error <- FALSE
+      pp <- tryCatch({
+        inverse_ramp_path(trial[["result"]])
+      }, error = function(e) had_error <<- TRUE
+      )
+      expect_false(had_error)
+      # Compare lists by comparing names and then values.
+      expect_equal(sort(names(pp)), sort(names(expected)))
+      expect_equal(pp[sort(names(pp))], expected[sort(names(expected))])
+    })
+  }
+}
+
+
+# List an output for specified inputs.
+# Ignore location and path arguments b/c we know their handling is simple.
+# A NULL means we expect a stop condition.
+# Trials are (3 x stage, 2 x project, 2 x user, 2 x rproject) = 24.
+trials <- list(
+  missing_stage = list(
+    stage = NULL, project = "hi", user = "me", rproject = "always", result = NULL
+  ),
+  stage_in = list(
+    stage = "input", project = NULL, user = NULL, rproject = NULL, result = "inputs"),
+  stage_out = list(
+    stage = "output", project = NULL, user = NULL, rproject = NULL, result = NULL),
+  stage_working = list(
+    stage = "working", project = NULL, user = NULL, rproject = NULL, result = NULL),
+  stage_working_user = list(
+    stage = "working", project = NULL, user = "ad", rproject = NULL, result = "users/ad"),
+  stage_in_p = list(
+    stage = "input", project = "ugp", user = NULL, rproject = NULL, result = "projects/ugp/inputs"),
+  stage_out_p = list(
+    stage = "output", project = "ugp", user = NULL, rproject = NULL, result = "projects/ugp/outputs"),
+  stage_working_p = list(
+    stage = "working", project = "ugp", user = NULL, rproject = NULL, result = NULL),
+  stage_working_p_u = list(
+    stage = "working", project = "ugp", user = "ad", rproject = NULL, result = "projects/ugp/users/ad"),
+  stage_working_r = list(
+    stage = "working", project = NULL, user = NULL, rproject = "rp", result = "libraries/rp/inst/extdata"),
+  stage_working_r_user = list(
+    stage = "working", project = NULL, user = "ad", rproject = "rp", result = "libraries/rp/inst/extdata/users/ad")
+)
+# These trials don't work in reverse because the stage is inferred as "working."
+forward_only_trials <- list(
+  stage_out_user = list(
+    stage = "output", project = NULL, user = "ad", rproject = NULL, result = "users/ad"),
+  stage_in_r = list(
+    stage = "input", project = NULL, user = NULL, rproject = "rp", result = "libraries/rp/inst/extdata"),
+  stage_out_r = list(
+    stage = "output", project = NULL, user = NULL, rproject = "rp", result = "libraries/rp/inst/extdata")
+)
+test_list(trials)
+test_inverse_list(trials)
+test_list(forward_only_trials)
